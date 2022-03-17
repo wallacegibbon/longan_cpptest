38b4562940d9fba1e2c73c48daf90c2210bd76ab
wallace <wallacegibbon@outlook.com>
2022-03-17 18:28:22 +0800

    Trivial changes: indent with 2-spaces now.

--- src/Color16_Screen.cpp
+++ src/Color16_Screen.cpp
@@ -2,26 +2,30 @@
 
 void Color16_Screen::fill(int x1, int y1, int x2, int y2, Color16 color)
 {
-	addr_set(x1, y1, x2, y2);
-	for (int i = y1; i <= y2; i++)
-		for (int j = x1; j <= x2; j++)
-			write_data(color);
+  addr_set(x1, y1, x2, y2);
+  for (int i = y1; i <= y2; i++)
+  {
+    for (int j = x1; j <= x2; j++)
+    {
+      write_data(color);
+    }
+  }
 }
 
 void Color16_Screen::clear(Color16 color)
 {
-	fill(0, 0, width - 1, height - 1, color);
+  fill(0, 0, width - 1, height - 1, color);
 }
 
 void Color16_Screen::draw_point_big(int x, int y, Color16 color)
 {
-	fill(x - 1, y - 1, x + 1, y + 1, color);
+  fill(x - 1, y - 1, x + 1, y + 1, color);
 }
 
 void Color16_Screen::draw_point(int x, int y, Color16 color)
 {
-	addr_set(x, y, x, y);
-	write_data(color);
+  addr_set(x, y, x, y);
+  write_data(color);
 }
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
@@ -31,93 +35,95 @@ void Color16_Screen::draw_point(int x, int y, Color16 color)
 class LinePainter
 {
 public:
-	LinePainter(Color16_Screen &target_screen, int x1, int y1, int x2, int y2, Color16 color1)
-	    : screen(target_screen), x(x1), y(y1), acc_x(0), acc_y(0), delta_x(x2 - x1), delta_y(y2 - y1), color(color1)
-	{
-		step_x = unit_value(delta_x);
-		step_y = unit_value(delta_y);
-		delta_x = abs(delta_x);
-		delta_y = abs(delta_y);
-		distance = max(delta_x, delta_y);
-	}
+  LinePainter(Color16_Screen &target_screen, int x1, int y1, int x2, int y2, Color16 color1)
+      : screen(target_screen), x(x1), y(y1), acc_x(0), acc_y(0), delta_x(x2 - x1), delta_y(y2 - y1), color(color1)
+  {
+    step_x = unit_value(delta_x);
+    step_y = unit_value(delta_y);
+    delta_x = abs(delta_x);
+    delta_y = abs(delta_y);
+    distance = max(delta_x, delta_y);
+  }
 
-	void draw_line();
+  void draw_line();
 
 private:
-	void draw_step();
+  void draw_step();
 
-	Color16_Screen &screen;
-	int delta_x;
-	int delta_y;
-	int distance;
-	int step_x;
-	int step_y;
-	int x;
-	int y;
-	int acc_x;
-	int acc_y;
-	Color16 color;
+  Color16_Screen &screen;
+  int delta_x;
+  int delta_y;
+  int distance;
+  int step_x;
+  int step_y;
+  int x;
+  int y;
+  int acc_x;
+  int acc_y;
+  Color16 color;
 };
 
 void LinePainter::draw_step()
 {
-	screen.draw_point(x, y, color);
-	acc_x += delta_x;
-	if (acc_x >= distance)
-	{
-		acc_x -= distance;
-		x += step_x;
-	}
-	acc_y += delta_y;
-	if (acc_y >= distance)
-	{
-		acc_y -= distance;
-		y += step_y;
-	}
+  screen.draw_point(x, y, color);
+  acc_x += delta_x;
+  if (acc_x >= distance)
+  {
+    acc_x -= distance;
+    x += step_x;
+  }
+  acc_y += delta_y;
+  if (acc_y >= distance)
+  {
+    acc_y -= distance;
+    y += step_y;
+  }
 }
 
 void LinePainter::draw_line()
 {
-	for (int i = 0; i <= distance; i++)
-		draw_step();
+  for (int i = 0; i <= distance; i++)
+  {
+    draw_step();
+  }
 }
 
 void Color16_Screen::draw_line(int x1, int y1, int x2, int y2, Color16 color)
 {
-	LinePainter p(*this, x1, y1, x2, y2, color);
-	p.draw_line();
+  LinePainter p(*this, x1, y1, x2, y2, color);
+  p.draw_line();
 }
 
 void Color16_Screen::draw_rectangle(int x1, int y1, int x2, int y2, Color16 color)
 {
-	draw_line(x1, y1, x2, y1, color);
-	draw_line(x2, y2, x1, y2, color);
+  draw_line(x1, y1, x2, y1, color);
+  draw_line(x2, y2, x1, y2, color);
 
-	draw_line(x1, y2, x1, y1, color);
-	draw_line(x2, y1, x2, y2, color);
+  draw_line(x1, y2, x1, y1, color);
+  draw_line(x2, y1, x2, y2, color);
 }
 
 void Color16_Screen::draw_point_x(int x, int y, int a, int b, Color16 color)
 {
-	draw_point(x - a, y + b, color);
-	draw_point(x + a, y - b, color);
+  draw_point(x - a, y + b, color);
+  draw_point(x + a, y - b, color);
 
-	draw_point(x - a, y - b, color);
-	draw_point(x + a, y + b, color);
+  draw_point(x - a, y - b, color);
+  draw_point(x + a, y + b, color);
 }
 
 void Color16_Screen::draw_circle(int x, int y, int r, Color16 color)
 {
-	int px = r;
-	int py = 0;
-	while (py <= px)
-	{
-		draw_point_x(x, y, py, px, color);
-		draw_point_x(x, y, px, py, color);
-		py++;
-		if ((py * py + px * px) > (r * r))
-		{
-			px--;
-		}
-	}
+  int px = r;
+  int py = 0;
+  while (py <= px)
+  {
+    draw_point_x(x, y, py, px, color);
+    draw_point_x(x, y, px, py, color);
+    py++;
+    if ((py * py + px * px) > (r * r))
+    {
+      px--;
+    }
+  }
 }
--- src/Color16_Screen_SPI.h
+++ src/Color16_Screen_SPI.h
@@ -7,31 +7,31 @@
 class Color16_Screen_SPI : public Color16_Screen
 {
 public:
-	Color16_Screen_SPI(int dev, int rst_port1, int rst_pin1, int cs_port1, int cs_pin1, int dc_port1, int dc_pin1, int width, int height)
-	    : Color16_Screen(width, height),
-	      spi_dev(dev), rst_port(rst_port1), rst_pin(rst_pin1),
-	      cs_port(cs_port1), cs_pin(cs_pin1), dc_port(dc_port1), dc_pin(dc_pin1) {}
+  Color16_Screen_SPI(int dev, int rst_port1, int rst_pin1, int cs_port1, int cs_pin1, int dc_port1, int dc_pin1, int width, int height)
+      : Color16_Screen(width, height),
+        spi_dev(dev), rst_port(rst_port1), rst_pin(rst_pin1),
+        cs_port(cs_port1), cs_pin(cs_pin1), dc_port(dc_port1), dc_pin(dc_pin1) {}
 
-	void spi_rst_set() { gpio_bit_set(rst_port, rst_pin); }
-	void spi_rst_clr() { gpio_bit_reset(rst_port, rst_pin); }
-	void spi_cs_set() { gpio_bit_set(cs_port, cs_pin); }
-	void spi_cs_clr() { gpio_bit_reset(cs_port, cs_pin); }
-	void spi_dc_set() { gpio_bit_set(dc_port, dc_pin); }
-	void spi_dc_clr() { gpio_bit_reset(dc_port, dc_pin); }
+  void spi_rst_set() { gpio_bit_set(rst_port, rst_pin); }
+  void spi_rst_clr() { gpio_bit_reset(rst_port, rst_pin); }
+  void spi_cs_set() { gpio_bit_set(cs_port, cs_pin); }
+  void spi_cs_clr() { gpio_bit_reset(cs_port, cs_pin); }
+  void spi_dc_set() { gpio_bit_set(dc_port, dc_pin); }
+  void spi_dc_clr() { gpio_bit_reset(dc_port, dc_pin); }
 
-	void write_data(uint16_t data) override;
-	void write_data8(uint8_t data);
-	void write_reg(uint8_t data);
+  void write_data(uint16_t data) override;
+  void write_data8(uint8_t data);
+  void write_reg(uint8_t data);
 
 private:
-	void write_bus(uint8_t data);
-	int spi_dev;
-	int rst_port;
-	int rst_pin;
-	int cs_port;
-	int cs_pin;
-	int dc_port;
-	int dc_pin;
+  void write_bus(uint8_t data);
+  int spi_dev;
+  int rst_port;
+  int rst_pin;
+  int cs_port;
+  int cs_pin;
+  int dc_port;
+  int dc_pin;
 };
 
 #endif
